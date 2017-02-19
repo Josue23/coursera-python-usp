ad97c279b1009f3ce8675ecb5037e54c71e70296
Josue23 <josuerodrigues@gmail.com>
2017-02-18 23:50:04 -0200

    programa1.py

--- programa1.py
+++ programa1.py
@@ -1,2 +1,2 @@
-print("Meu primeiro programa soma os números 2 e 3:")
-print("%d + %d = %d " %(2, 3, 2 + 3))
+print("Meu primeiro programa soma os números 2 e 5:")
+print("%d + %d = %d " %(2, 5, 2 + 5))
